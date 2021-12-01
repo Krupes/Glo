6d279ae4db31bdfcb5380f6a4401d5e650ca1090
Nikita Krupenko <smasher379zl@gmail.com>
2021-12-01 18:19:48 +0300

    Выполнил домашнее задание Ðномер 2

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-let title;
-let screens;
-let screenPrice ;
-let rollback;
-let fullPrice ;
-let adaptive;
+let title = 'Lesson 02';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 379;
+let rollback = 37;
+let fullPrice = 2000;
+let adaptive = true;
 
-alert('Hello,world!');
-console.log('Hello,GIT!');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + fullPrice +' рублей/ долларов/гривен/юани');
+console.log(screens.toLowerCase().split(', '));
+console.log('Процент отката посреднику за работу: '+ fullPrice*(rollback /  100));
+
